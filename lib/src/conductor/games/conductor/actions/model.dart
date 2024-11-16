b9e20b451cbc2411a537b006b9ac3379b77860c2
ark4579 <ark4579@hotmail.com>
2024-11-16 14:09:34 +0500

    updates

--- lib/src/conductor/games/conductor/actions/model.dart
+++ lib/src/conductor/games/conductor/actions/model.dart
@@ -1,86 +1,86 @@
-import 'package:conductor/conductor.dart';
+// import 'package:conductor/conductor.dart';
 
-class ItemSelectedAction<T extends ModelBase> extends CAction {
-  final T? item;
+// class ItemSelectedAction<T extends ModelBase> extends CAction {
+//   final T? item;
 
-  ItemSelectedAction({required this.item});
+//   ItemSelectedAction({required this.item});
 
-  @override
-  List<CTransition> get transitions => [
-        ItemSelectedTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         ItemSelectedTransition(triggererAction: this),
+//       ];
+// }
 
-class UpdateIfSelectedAction<T extends ModelBase> extends CAction {
-  final T? item;
+// class UpdateIfSelectedAction<T extends ModelBase> extends CAction {
+//   final T? item;
 
-  UpdateIfSelectedAction({required this.item});
+//   UpdateIfSelectedAction({required this.item});
 
-  @override
-  List<CTransition> get transitions => [
-        UpdateIfSelectedTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         UpdateIfSelectedTransition(triggererAction: this),
+//       ];
+// }
 
-class NewItemAction<T extends ModelBase> extends CAction {
-  @override
-  List<CTransition> get transitions => [
-        NewItemTransition(triggererAction: this),
-      ];
-}
+// class NewItemAction<T extends ModelBase> extends CAction {
+//   @override
+//   List<CTransition> get transitions => [
+//         NewItemTransition(triggererAction: this),
+//       ];
+// }
 
-class CreateItemAction<T extends ModelBase> extends CAction {
-  final T? item;
-  final bool refreshBeforeAdding;
-  final String? existingId;
+// class CreateItemAction<T extends ModelBase> extends CAction {
+//   final T? item;
+//   final bool refreshBeforeAdding;
+//   final String? existingId;
 
-  CreateItemAction({
-    required this.item,
-    this.refreshBeforeAdding = false,
-    this.existingId,
-  });
+//   CreateItemAction({
+//     required this.item,
+//     this.refreshBeforeAdding = false,
+//     this.existingId,
+//   });
 
-  @override
-  List<CTransition> get transitions => [
-        CreateItemTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         CreateItemTransition(triggererAction: this),
+//       ];
+// }
 
-class UpdateItemAction<T extends ModelBase> extends CAction {
-  final T? item;
+// class UpdateItemAction<T extends ModelBase> extends CAction {
+//   final T? item;
 
-  UpdateItemAction({
-    required this.item,
-  });
+//   UpdateItemAction({
+//     required this.item,
+//   });
 
-  @override
-  List<CTransition> get transitions => [
-        UpdateItemTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         UpdateItemTransition(triggererAction: this),
+//       ];
+// }
 
-class DeleteItemAction<T extends ModelBase> extends CAction {
-  final T? item;
+// class DeleteItemAction<T extends ModelBase> extends CAction {
+//   final T? item;
 
-  DeleteItemAction({
-    required this.item,
-  });
+//   DeleteItemAction({
+//     required this.item,
+//   });
 
-  @override
-  List<CTransition> get transitions => [
-        DeleteItemTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         DeleteItemTransition(triggererAction: this),
+//       ];
+// }
 
-class RefreshItemAction<T extends ModelBase> extends CAction {
-  final T? item;
+// class RefreshItemAction<T extends ModelBase> extends CAction {
+//   final T? item;
 
-  RefreshItemAction({
-    required this.item,
-  });
+//   RefreshItemAction({
+//     required this.item,
+//   });
 
-  @override
-  List<CTransition> get transitions => [
-        RefreshItemTransition(triggererAction: this),
-      ];
-}
+//   @override
+//   List<CTransition> get transitions => [
+//         RefreshItemTransition(triggererAction: this),
+//       ];
+// }
